fb7776c77e68604a4b26fc83a3adb581df797e82
Michael Arufe <marufe@compassmsp.com>
2026-05-19 17:13:27 -0400

    Create function Test-FileExists

---
+++ Test-FileExists.ps1
@@ -0,0 +1,21 @@
+function Test-FileExists {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$Path
+    )
+
+    try {
+        # Validate path explicitly as a file (not a directory)
+        if (Test-Path -Path $Path -PathType Leaf) {
+            return $true
+        }
+        else {
+            return $false
+        }
+    }
+    catch {
+        return $false
+    }
+}
+``
